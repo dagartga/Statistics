cefd455ae586dd4ff800ff211e123f208a316f53
Dagart Allison <65997824+dagartga@users.noreply.github.com>
2024-04-10 18:21:06 -0700

    place pct print at bottom

--- chi2_smoking_group_6_35.py
+++ chi2_smoking_group_6_35.py
@@ -8,13 +8,13 @@ patch_support_smoke = 110
 patch_no_support_quit = 30
 patch_no_support_smoke = 120
 
-# print the percent that quit in each group
-print(f"Percent that quit with support group: {round(patch_support_quit / (patch_support_quit + patch_support_smoke) * 100)}%")
-print(f"Percent that quit without support group: {round(patch_no_support_quit / (patch_no_support_quit + patch_no_support_smoke) * 100)}%")
-
 # create the contingency table
-obs = np.array([[patch_support_quit, patch_support_smoke],
-                [patch_no_support_quit, patch_no_support_smoke]])
+obs = np.array(
+    [
+        [patch_support_quit, patch_support_smoke],
+        [patch_no_support_quit, patch_no_support_smoke],
+    ]
+)
 
 # perform the chi-square test
 chi2_stat, p_val, dof, ex = chi2_contingency(obs)
@@ -26,3 +26,12 @@ print(f"P-Value: {p_val}")
 alpha = 0.05
 critical_value = chi2.ppf(1 - alpha, dof)
 print(f"Critical Value: {critical_value}")
+
+
+# print the percent that quit in each group
+print(
+    f"Percent that quit with support group: {round(patch_support_quit / (patch_support_quit + patch_support_smoke) * 100)}%"
+)
+print(
+    f"Percent that quit without support group: {round(patch_no_support_quit / (patch_no_support_quit + patch_no_support_smoke) * 100)}%"
+)
