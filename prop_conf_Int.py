e6f91855ab7500e2d6c17cfbc3ee864d6f9bc083
Dagart <dagartga@gmail.com>
2024-04-11 23:14:55 -0700

    create confidence interval for proportion

---
+++ prop_conf_Int.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+# number of users who visited the site
+n = 752
+
+# number of users who registered
+n_registered = 64
+
+# get the proportion of users who registered
+prop_registered = n_registered / n
+
+# get the standard error
+se = np.sqrt(prop_registered * (1 - prop_registered) / n)
+
+# use a z-score of 1.645 for a 90% confidence interval
+z = 1.645
+
+# calculate the margin of error
+moe = z * se
+
+# calculate the lower and upper bounds of the confidence interval
+ci_lower = prop_registered - moe
+ci_upper = prop_registered + moe
+
+print(f"Proportion of users who registered: {prop_registered}")
+print(f"With a confidence interval of 90%: {round(ci_lower, 3)} to {round(ci_upper, 3)}")
+
+
+from statsmodels.stats.proportion import proportion_confint
+
+ci_low, ci_upp = proportion_confint(n_registered, n, alpha=0.1, method='normal')
+
+print(f"90% Confidence Interval: {ci_low} to {ci_upp}")
