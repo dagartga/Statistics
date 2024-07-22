bccc5c4363940d585b8d59f40d23be991edf36b5
Dagart <dagartga@gmail.com>
2024-07-22 12:40:19 -0700

    format using black and add constant var format

--- ttest_power.py
+++ ttest_power.py
@@ -2,27 +2,43 @@ import numpy as np
 import statsmodels.stats.power as smp
 
 # Control group data
-control_size = 100
-control_mean = 74.1
-control_std = 20
+CONTROL_SIZE = 100
+CONTROL_MEAN = 74.1
+CONTROL_STD = 20
 
 # Treatment group data
-treatment_size = 100
-treatment_mean = 79.4
-treatment_std = 14
-
-# Calculating pooled standard deviation
-pooled_std = np.sqrt(((control_size - 1) * control_std ** 2 + (treatment_size - 1) * treatment_std ** 2) / (control_size + treatment_size - 2))
-
-# Calculating effect size
-effect_size = (treatment_mean - control_mean) / pooled_std
+TREATMENT_SIZE = 100
+TREATMENT_MEAN = 79.4
+TREATMENT_STD = 14
 
 # Significance level (alpha)
-alpha = 0.05
+ALPHA = 0.05
+
+# Calculating pooled standard deviation
+pooled_std = np.sqrt(
+    ((CONTROL_SIZE - 1) * CONTROL_STD**2 + (TREATMENT_SIZE - 1) * TREATMENT_STD**2)
+    / (CONTROL_SIZE + TREATMENT_SIZE - 2)
+)
+
+# Calculating effect size
+effect_size = (TREATMENT_MEAN - CONTROL_MEAN) / pooled_std
+
 
 # Calculating power
-power = smp.tt_ind_solve_power(effect_size=effect_size, nobs1=control_size, alpha=alpha, ratio=treatment_size/control_size, alternative='larger')
+power = smp.tt_ind_solve_power(
+    effect_size=effect_size,
+    nobs1=CONTROL_SIZE,
+    alpha=ALPHA,
+    ratio=TREATMENT_SIZE / CONTROL_SIZE,
+    alternative="larger",
+)
 
 print("Power of the t-test:", power)
-print("The power of the t-test is the probability \nof correctly rejecting the null hypothesis \nwhen the alternative hypothesis is true.")
-print("The probability of incorrectly failing to \nreject the null hypothesis is {0:.2f}".format(1 - power))
+print(
+    "The power of the t-test is the probability \nof correctly rejecting the null hypothesis \nwhen the alternative hypothesis is true."
+)
+print(
+    "The probability of incorrectly failing to \nreject the null hypothesis is {0:.2f}".format(
+        1 - power
+    )
+)
